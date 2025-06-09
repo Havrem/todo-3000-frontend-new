a7c1ecf18174608aa1c2361199ea2345f3a0d454
Havrem <144966173+Havrem@users.noreply.github.com>
2025-06-09 17:41:49 +0200

    Touch up dashboard small & large

--- src/components/DashboardSmall.tsx
+++ src/components/DashboardSmall.tsx
@@ -13,7 +13,7 @@ export const DashboardSmall = () => {
     const upcoming = todos
             .filter(todo => !todo.completed)
             .sort((a, b) => new Date(a.due).getTime() - new Date(b.due).getTime())
-            .slice(0, 2);
+            .slice(0, 6);
 
     useEffect(() => {
         if (isError) {
@@ -38,21 +38,22 @@ export const DashboardSmall = () => {
             <div className={styles.top}>
                 <img src={imageSrcUrl} className={styles.avatar}/>
                 <div className={styles.right}>
-                    <p>Completed: {completed}</p>
-                    <p>Remaining: {remaining}</p>
+                    <h2>Statistic</h2>
+                    <div className={styles.statistic}>
+                        <p>Completed: {completed}</p>
+                        <p>Remaining: {remaining}</p>
+                    </div>
                 </div>
             </div>
 
             <div className={styles.bottom}>
                 <div className={styles.left}>
                     <div className={styles.upcoming}>
-                        <label>Upcoming</label>
+                        <h2>Upcoming</h2>
 
-                        {upcoming.length == 0 ? 
-                        (
-                            <p></p>
-                        ) :
-                        (
+                        {upcoming.length === 0 ? (
+                            <p>No upcoming tasks.</p>
+                        ) : (
                             <div className={styles.list}>
                             {upcoming.map((todo) => {
                                 return (
@@ -65,6 +66,8 @@ export const DashboardSmall = () => {
                         </div>
                         )}
                     </div>
+                </div>
+                <div className={styles.right}>
                 </div>
             </div>
         </div>
